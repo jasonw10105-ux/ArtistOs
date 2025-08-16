a088a35f613013cef8e4b875aca0f55149d5189c
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-16 20:11:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MarketingPage from './components/Marketing.jsx'
+import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
 import SetPassword from './components/SetPassword.jsx'
 import UploadWork from './components/UploadWork.jsx'
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/set-password" element={<SetPassword />} />
-        <Route path="/upload" element={<UploadWork />} />
-        {/* add other routes as needed */}
-      </Routes>
-    </Router>
-  )
-}
+<Routes>
+  <Route path="/" element={<MarketingPage />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/signup" element={<Signup />} />
+  <Route path="/set-password" element={<SetPassword />} />
+  <Route path="/upload" element={<UploadWork />} />
+</Routes>
