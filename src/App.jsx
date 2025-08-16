b59f308cb7bb563ef318c6ee9efa7c862fa2299c
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-16 20:18:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import MarketingPage from './components/Marketing.jsx'
-import Login from './components/Login.jsx'
-import Signup from './components/Signup.jsx'
-import SetPassword from './components/SetPassword.jsx'
-import UploadWork from './components/UploadWork.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Marketing from './components/Marketing'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import SetPassword from './components/SetPassword'
+import Dashboard from './components/Dashboard'
+import UploadWork from './components/UploadWork'
+import Catalogue from './components/Catalogue'
+import AccountSettings from './components/AccountSettings'
+import OrderTracking from './components/OrderTracking'
+import InquiryTracking from './components/InquiryTracking'
+import SalesTracking from './components/SalesTracking'
 
-<Routes>
-  <Route path="/" element={<MarketingPage />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/signup" element={<Signup />} />
-  <Route path="/set-password" element={<SetPassword />} />
-  <Route path="/upload" element={<UploadWork />} />
-</Routes>
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Marketing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/set-password" element={<SetPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/uploadwork" element={<UploadWork />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/accountsettings" element={<AccountSettings />} />
+        <Route path="/ordertracking" element={<OrderTracking />} />
+        <Route path="/inquirytracking" element={<InquiryTracking />} />
+        <Route path="/salestracking" element={<SalesTracking />} />
+      </Routes>
+    </Router>
+  )
+}
