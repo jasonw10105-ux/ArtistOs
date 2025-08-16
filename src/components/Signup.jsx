aef93d4c02a7adcf267af1e9c81243fc67f150f2
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-16 20:05:30 +0200

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { supabase } from '../lib/supabaseClient'
+import { supabase } from '../utils/supabaseClient'
 
 export default function Signup() {
   const [email, setEmail] = useState('')
