5acb758a169efc218689c5677fe652e43c0d3be0
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-17 19:39:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
-  }
-});
+  base: "/",  // 👈 IMPORTANT
+})
